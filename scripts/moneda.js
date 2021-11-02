19f81b6acf50b0222bde78a8d7f0c8d170c3834b
Daniel Lorenzo <juandaniellorenzocabrera@alumno.ieselrincon.es>
2021-11-02 14:48:05 +0000

    Chequeo de selección teminado, falta desarrollo de partida

--- scripts/moneda.js
+++ scripts/moneda.js
@@ -1,25 +1,14 @@
 const partida = {
     monedaJugador : "",
+    caraMonedaJugador: "",
     monedaOrdenador : "",
+    caraMonedaOrdenador: "",
     victoriasJugador : 0,
     victoriasOrdenador : 0,
-    partidasJugadas : 0
+    partidasJugadas : 0,
 }
 
-const monedas = {
-    moneda1 : {
-        cara : "src...",
-        cruz : "src..."
-    },
-    moneda2 : {
-        cara : "src...",
-        cruz : "src..."
-    },
-    moneda3 : {
-        cara : "src...",
-        cruz : "src..."
-    }
-}
+
 
 
 
@@ -37,17 +26,49 @@ document.addEventListener("DOMContentLoaded" , ()=>{
 
 
 function comenzarPartida(){
-    haElegidoMoneda();
+    if(partida.monedaJugador === "" || partida.caraMonedaJugador === ""){
+        alert("Debes seleccionar un tipo de moneda y la cara que deseas jugar");
+    } else{
+        
+    }
 }
 
-
+/*
 function haElegidoMoneda(){
-    const seleccionUsuario =  document.querySelector(".seleccionada") ? true : false;
+    const seleccionUsuario =  partida.monedaJugador && partida.caraMonedaJugador ? true : false;
     return seleccionUsuario;
 }
-
+*/
 
 function elegirTipoMoneda(){
+    // Si hay uno seleccionado le quitamos la clase seleccionado
     const seleccionAnterior = document.querySelector('.seleccionada') ? (document.querySelector('.seleccionada')).classList.toggle("seleccionada") : '';
-    this.classList.toggle("seleccionada");
+    this.classList.toggle("seleccionada"); // Le ponemos la clase al seleccionado
+    partida.monedaJugador = this.dataset.tipomoneda;
+    partida.monedaOrdenador = this.dataset.tipomoneda;
+    mostrarCarasMoneda();
+
+}
+
+function mostrarCarasMoneda(){
+    let seleccionCara =  document.querySelector(".seleccion-cara");
+    let carasMoneda = seleccionCara.querySelectorAll("img");
+    carasMoneda[0].src = `./../imagenes/${partida.monedaJugador}_cara.png`;
+    carasMoneda[0].dataset.caraMonedaJugador = `cara`;
+    carasMoneda[1].src = `./../imagenes/${partida.monedaJugador}_cruz.png`;
+    carasMoneda[1].dataset.caraMonedaJugador = `cruz`;
+    carasMoneda.forEach(moneda => moneda.addEventListener("click", elegirCaraMoneda));
+}
+
+function elegirCaraMoneda(){
+    if(this.dataset.caraMonedaJugador === "cara"){
+        partida.caraMonedaJugador = "cara";
+        partida.caraMonedaOrdenador = "cruz";
+    }else{
+        partida.caraMonedaJugador = "cruz";
+        partida.caraMonedaOrdenador = "cara";
+    }
+    let monedaOrdenador =  document.querySelector("#imagenOrdenador");
+    monedaOrdenador.src = `./../imagenes/${partida.monedaOrdenador}_${partida.caraMonedaOrdenador}.png`
+    console.log(monedaOrdenador);
 }
