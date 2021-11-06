1903ff16ae3c8f51229697003fbfcfac5d6d2ef9
Daniel Lorenzo <juandaniellorenzocabrera@alumno.ieselrincon.es>
2021-11-06 13:30:08 +0000

    Finalizado piedra papel tijeras, comenzamos funciones

--- scripts/piedraPapelTijeras.js
+++ scripts/piedraPapelTijeras.js
@@ -2,28 +2,24 @@
 const partida = {
     manoJugador : "",
     manoOrdenador: "",
-    manos : ["piedra","papel", "tijeras"]
+    manos : ["piedra","papel", "tijeras"],
+    victoriasJugador : 0,
+    victoriasOrdenador : 0,
+    partidasJugadas : 0
     
 }
 
 
-
-
 document.addEventListener("DOMContentLoaded", ()=>{
 
+    let contenerdorManos = document.querySelector(".manos");
+    let manos = contenerdorManos.querySelectorAll("img");
+    manos.forEach(mano => mano.addEventListener("click", seleccionarMano));
 
-let contenerdorManos = document.querySelector(".manos");
-let manos = contenerdorManos.querySelectorAll("img");
-manos.forEach(mano => mano.addEventListener("click", seleccionarMano));
+    let comenzarJuego =  document.querySelector("#comenzarJuego");
+    comenzarJuego.addEventListener("click", comenzarPartida);
 
-
-let comenzarJuego =  document.querySelector("#comenzarJuego");
-comenzarJuego.addEventListener("click", piedraPapelTijeras)
-
-
-
-
-})
+});
 
 
 function seleccionarMano(){
@@ -34,23 +30,44 @@ function seleccionarMano(){
 }
 
 
-function piedraPapelTijeras(){
-    if(partida.manoJugador === "" || partida.manoOrdenador === ""){
-        alert("Debes de seleccionar una mano");
-        console.table(partida)
-    }else{
-        mostrarManos();
-    }
+function comenzarPartida(){
+    partida.manoJugador === "" || partida.manoOrdenador === "" ? alert("Debes de seleccionar una mano"): mostrarManos();
 }
 
 
 function mostrarManos(){
-    let empate = partida.manoOrdenador === partida.manoJugador;
-    let ganaJugador = [
-        `${partida.manoJugador} === "piedra" && ${partida.manoOrdenador} === "tijeras`,
-        `${partida.manoJugador} === "tijeras" && ${partida.manoOrdenador} === "papel"`,
-        `${partida.manoJugador} === "papel" && ${partida.manoOrdenador} === "piedra"`,
-    ]
-    
+    (document.querySelector("#manoJugador")).src = `./imagenes/${partida.manoJugador}.svg`;
+    (document.querySelector("#manoOrdenador")).src = `./imagenes/${partida.manoOrdenador}.svg`;
+    (document.querySelector(".versus")).innerHTML = "VS";
+    asignarGanador();
+    partida.manoJugador = "";
+    partida.manoOrdenador = "";
+    actualizarMarcador();
 
+}
+
+
+function asignarGanador(){
+    let empate = partida.manoOrdenador === partida.manoJugador ? true : false;
+    let ganaJugador = partida.manoJugador === "piedra" && partida.manoOrdenador === "tijeras" || 
+        partida.manoJugador === "tijeras" && partida.manoOrdenador === "papel" ||
+        partida.manoJugador === "papel" && partida.manoOrdenador === "piedra" ? true : false;
+
+    let resultado = document.querySelector(".resultado");
+    
+    if(empate){
+        resultado.innerHTML = "Ha sido empate";
+    }else if(ganaJugador){
+        resultado.innerHTML = "Has ganado";
+        partida.victoriasJugador++;
+    }else{
+        resultado.innerHTML = "Gana el ordenador, tú pierdes";
+        partida.victoriasOrdenador++;
+    }
+}
+
+
+function actualizarMarcador(){
+    (document.querySelector("#victoriasJugador")).innerHTML = `Victorias: ${partida.victoriasJugador}`;
+    (document.querySelector("#victoriasOrdenador")).innerHTML = `Victorias: ${partida.victoriasOrdenador}`;
 }
